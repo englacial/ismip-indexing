274771ffa166c836925b23718bc05eb5681356af
Shane Grigsby <refuge@rocktalus.com>
2026-01-28 21:11:07 -0800

    minor fixes, aws paths

--- js-viewer/vite.config.ts
+++ js-viewer/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/viewer/",
+  base: "/static/models/",
   build: {
     outDir: "dist",
     sourcemap: true,
